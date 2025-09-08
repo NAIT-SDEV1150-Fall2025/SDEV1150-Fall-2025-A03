03e6fe00c28bc98f388d967de9a1c0b50f2a471d
Daniel Mouris <dgmouris@gmail.com>
2025-09-08 09:47:48 -0600

    class progress pushed

--- lesson-01-starter/js/main.js
+++ lesson-01-starter/js/main.js
@@ -1,1 +1,8 @@
 console.log('Lesson 01 starter loaded');
+
+// we're going to touch on this piece
+// that we're going to use css selectors
+let title = document.querySelector("h1") //h1 is selecting that element
+
+// I'm going to modify it
+title.innerText = "Modified from JavaScript"
