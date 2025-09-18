03b8c027f304bc5c5420029cfa7d6d7e473b2eac
Daniel Mouris <dgmouris@gmail.com>
2025-09-18 08:46:12 -0600

    class progress pushed

--- lesson-03-starter/src/main.js
+++ lesson-03-starter/src/main.js
@@ -7,7 +7,9 @@ let count = 42;
 const isActive = true;
 
 // 2. Log their types with console.log(typeof …)
-console.log(`Type of greeting: ${ typeof greeting}`);
+console.log(`Type of greeting: ${typeof greeting}`);
+console.log(`Type of count: ${typeof count}`);
+console.log(`Type of isActive: ${typeof isActive}`);
 
 // 3. Try built-in functions: alert(), prompt(), parseInt(), toString()
 // 4. Manipulate values and observe results in the console
