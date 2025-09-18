8b2523b6c07a23354e52d835577658dc2670941d
Daniel Mouris <dgmouris@gmail.com>
2025-09-18 08:48:24 -0600

    class progress pushed

--- lesson-03-starter/src/main.js
+++ lesson-03-starter/src/main.js
@@ -12,6 +12,10 @@ console.log(`Type of count: ${typeof count}`);
 console.log(`Type of isActive: ${typeof isActive}`);
 
 // 3. Try built-in functions: alert(), prompt(), parseInt(), toString()
+const userName = prompt("enter your name");
+const continueDemo = confirm(`Hi ${userName} do you want to continue? `);
+console.log(`${userName} chose "${continueDemo}" to continue`);
+
 // 4. Manipulate values and observe results in the console
 
 // Student TODO:
