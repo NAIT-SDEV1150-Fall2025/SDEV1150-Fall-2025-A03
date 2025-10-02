a3919fb037d072b6326a8330c56dfb06f6b6c667
Daniel Mouris <dgmouris@gmail.com>
2025-10-02 09:39:25 -0600

    class progress pushed

--- lesson-08-starter/src/main.js
+++ lesson-08-starter/src/main.js
@@ -6,16 +6,26 @@ console.log('Lesson 08 starter loaded');
 window.addEventListener('DOMContentLoaded', () => {
   console.log('Fired on mounting of the page');
   // 2. Selecting elements
-  /* in two mins I want you to select the items with the following ids:
-  btn-toggle
-  btn-message
-  message
-  hover-card
-  hover-status
-  key-output
-  list
-  selection
+  /*
+    in two mins I want you to select the items with the following ids:
+    btn-toggle
+    btn-message
+    message
+    hover-card
+    hover-status
+    key-output
+    list
+    selection
   */
+  let btnToggle = document.querySelector('#btn-toggle');
+  let btnMessage = document.querySelector('#btn-message');
+  let message = document.querySelector('#message');
+  let hoverCard = document.querySelector('#hover-card');
+  let hoverStatus = document.querySelector('#hover-status');
+  let keyOutput = document.querySelector('#key-output');
+  let list = document.querySelector('#list');
+  let selection = document.querySelector('#selection');
+  console.log({ btnToggle, btnMessage, message, hoverCard, hoverStatus, keyOutput, list, selection });
 
   // 3. click: toggle a highlight class on the body
 
