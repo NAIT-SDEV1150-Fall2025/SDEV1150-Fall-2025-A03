c4cf76ffb5c79e768d9bf0ee9641a1539bcb2aba
Daniel Mouris <dgmouris@gmail.com>
2025-10-16 08:34:02 -0600

    class progress pushed

--- lesson-10-starter/src/main.js
+++ lesson-10-starter/src/main.js
@@ -1,12 +1,17 @@
 console.log('Lesson 10 starter loaded');
 
 // 1. Select required elements
+const form = document.querySelector('#contact-form');
+const result = document.querySelector('#result');
 
 // 2. Function to gather and structure form data
 
 // Access values using both form.elements and query selectors
 
 // 3. Handle form submission
+
+
+
 // Use 'submit' event on the form, not 'click' on the button
 // Prevent default behavior (navigation/reload) using event.preventDefault()
 
