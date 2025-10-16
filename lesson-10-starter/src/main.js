a05eff3e983cccc24f5b164087d3b73c9da852ac
Daniel Mouris <dgmouris@gmail.com>
2025-10-16 09:07:12 -0600

    class progress pushed

--- lesson-10-starter/src/main.js
+++ lesson-10-starter/src/main.js
@@ -12,23 +12,32 @@ function serializeData(formElement) {
   // get the values from the inputs so we can use
   // them in our application.
   // let's focus on text based inputs first.
-  let fullName = formElement.elements.fullName;
+  // let fullName = formElement.elements.fullName;
+
   // for formElement.elements.fullName, "fullName"
   // is the "name" attribute on the input.
   // if you want to see what the user entered into
   // the input element you use .value on the inpu.
   // console.log(fullName); // the same thing that you would get from a query selector.
-  console.log(`Fullname value is: ${fullName.value}`);
+  // console.log(`Fullname value is: ${fullName.value}`);
 
   // get the email and the bio print it out to the console.
   // let's do email first
-  let email = formElement.elements.email;
-  console.log(`email is: ${email.value}`);
+  // let email = formElement.elements.email;
+  // console.log(`email is: ${email.value}`);
   // bio.
-  let bio = formElement.elements.bio;
-  console.log(`bio is: ${bio.value}`);
+  // let bio = formElement.elements.bio;
+  // console.log(`bio is: ${bio.value}`);
 
   // at the we're going to return everything in an object.
+  // object destructuring.
+  // we can create variables based on the name using
+  // the following technique you don't need to use this
+  // if it's confusing just the above.
+  let { fullName, bio, email } = formElement.elements;
+  console.log(`Fullname value is: ${fullName.value}`);
+  console.log(`email is: ${email.value}`);
+  console.log(`bio is: ${bio.value}`);
 }
 
 // Access values using both form.elements and query selectors
