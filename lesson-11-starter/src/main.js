46e82767df6308e88524dc3cc1ded18fb36d7dc4
Daniel Mouris <dgmouris@gmail.com>
2025-10-20 09:28:33 -0600

    class progress pushed

--- lesson-11-starter/src/main.js
+++ lesson-11-starter/src/main.js
@@ -69,10 +69,20 @@ form.addEventListener('input', (event) => {
   }
 
   // 1.2 custom validation for bio (minimum length)
-  // do the same thing but I want you to make sure that the length
-  // of the value of the bio is more than 40 chars
-  // you'll have to perform similar steps as above.
-
+  // the first thing we're going to do is check for the name
+  if (element.name === 'bio') {
+    // do the same thing but I want you to make sure that the length
+    // of the value of the bio is more than 40 chars
+    if (element.value.length < 40) {
+      // strings in javascript are character arrays (which means we can use .length)
+      // this is the invalid case
+      element.setCustomValidity('Bio needs to be longer than 40 chars');
+    } else {
+      // this is the valid case
+      element.setCustomValidity('');
+    }
+    // you'll have to perform similar steps as above.
+  }
 
   // 1.3 custom validation for email (basic pattern check)
 
