45055e51ea146a511fa9c69c722d0f6156a8e4bf
Daniel Mouris <dgmouris@gmail.com>
2025-10-23 09:25:47 -0600

    class progress pushed

--- lesson-13-starter/main.js
+++ lesson-13-starter/main.js
@@ -1,1 +1,16 @@
 // let's use our new package.
+// taking a look at now docs: https://day.js.org/docs/en/parse/now
+import dayjs from 'dayjs'
+// the modern way to import js modules.
+
+// let's get todays date
+let now = dayjs();
+
+// let's format this string to show the current day and time
+console.log('A Small node example');
+// using the docs here: https://day.js.org/docs/en/display/format
+// let's display date and time.
+console.log('the date is');
+console.log(now.format('YYYY-MM-DD'));
+console.log('the time is');
+console.log(now.format('HH-mm-ss'));
