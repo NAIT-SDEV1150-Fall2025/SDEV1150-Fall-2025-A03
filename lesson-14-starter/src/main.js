bf2cd93f16a4780841c8349e5e59db04c4f64b0c
Daniel Mouris <dgmouris@gmail.com>
2025-10-27 08:35:44 -0600

    class progress pushed

--- lesson-14-starter/src/main.js
+++ lesson-14-starter/src/main.js
@@ -1,6 +1,15 @@
 console.log('Lesson 14 starter loaded');
 
 // make it so that we can use this function
-import { greetUser } from './utils';
+import {
+  greetUser,
+  getDate
+} from './utils';
 
 console.log(greetUser('Dan'));
+
+let dateEl = document.querySelector('#today');
+
+dateEl.textContent = getDate();
+
+
