3f6dbd820bec9b6a61bcfbbe64b43446613ff118
Daniel Mouris <dgmouris@gmail.com>
2025-10-27 08:24:28 -0600

    class progress pushed

--- lesson-14-starter/src/utils.js
+++ lesson-14-starter/src/utils.js
@@ -6,6 +6,16 @@ function greetUser(name) {
   return `Welcome to the app ${name}`;
 }
 
+// I want you to stop the server with ctrl c
+// install dayjs
+// I want you to create a function called getDate
+// I want you to return the formatted date (date only)
+// export this, import in the main.js
+// I want you to update the html with the current date
+// (where it says today)
+
+
+
 // with export I'm going to make this function
 // importable to another function.
 export { greetUser };
