210ca42801e9d7dde254e0027fe1a6ff79987f30
Daniel Mouris <dgmouris@gmail.com>
2025-10-27 08:33:56 -0600

    class progress pushed

--- lesson-14-starter/src/utils.js
+++ lesson-14-starter/src/utils.js
@@ -1,3 +1,6 @@
+// import dayjs so that I can use it in my code.
+import dayjs from 'dayjs';
+
 // we're going to create/define a few functions
 // to use inside of our main.js
 // a really common pattern is to have your function
@@ -9,6 +12,12 @@ function greetUser(name) {
 // I want you to stop the server with ctrl c
 // install dayjs
 // I want you to create a function called getDate
+// Note: you can use export in front of the function
+// definition and it will be have the same way as the
+// other export method.
+export function getDate() {
+  return dayjs().format('DD/MM/YYYY');
+}
 // I want you to return the formatted date (date only)
 // export this, import in the main.js
 // I want you to update the html with the current date
