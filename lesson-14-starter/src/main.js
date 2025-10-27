8383ab4019c5fe31b6add60bb32fd0a8a53c8ca0
Daniel Mouris <dgmouris@gmail.com>
2025-10-27 09:12:27 -0600

    class progress pushed

--- lesson-14-starter/src/main.js
+++ lesson-14-starter/src/main.js
@@ -1,4 +1,6 @@
 console.log('Lesson 14 starter loaded');
+// let's import the animate from animejs
+import { animate } from 'animejs'
 
 // make it so that we can use this function
 import {
@@ -19,3 +21,14 @@ dateEl.textContent = getDate();
 timeEl.textContent = getTime();
 // let's just our greeting
 greetEl.textContent = greetUser('Dan');
+
+// we're going to use
+animate(
+  '#greet', // first argument is the target.
+  {
+    translateY: [-20, 0],
+    opacity: [0, 1],
+    duration: 5000,
+    easing: 'easeOutQuad',
+  },
+);
