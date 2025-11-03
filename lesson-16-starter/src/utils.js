cd671b5e3118146807b0c35c706548a76b286348
Daniel Mouris <dgmouris@gmail.com>
2025-11-03 09:18:28 -0700

    class progress pushed

--- lesson-16-starter/src/utils.js
+++ lesson-16-starter/src/utils.js
@@ -25,5 +25,27 @@ export async function getData(endpoint) {
 }
 
 // POST utility function
+export async function postData(endpoint, payload) {
+  // make the post request
+  const response = await fetch(endpoint, {
+    method: 'POST', // we're creating new info on the server
+    headers: {
+      'Content-Type': 'application/json',
+      // this give info to the server that we're giving json
+      // this is going to be you'll pass tokens (for auth)
+    },
+    body: JSON.stringify(payload),
+    // what you're giving to the server
+  });
+  // check if ti was okay.
+  if (!response.ok) {
+    throw new Error('Network response failed');
+  }
+  // on the response we can't guarantee that the json
+  // is well formed, there's a method as well await
+  // which converting the json to a js object
+  const data = await response.json();
+  return data;
+}
 
 // TODO: Add DELETE function here
