8cadd634198952feda218cf175713a19fda4a7ad
Daniel Mouris <dgmouris@gmail.com>
2025-11-13 09:26:23 -0700

    class progress pushed

--- lesson-19-starter/src/user-card.js
+++ lesson-19-starter/src/user-card.js
@@ -16,6 +16,16 @@ class UserCard extends HTMLElement {
     const template = document.getElementById('user-card-template');
     const content = template.content.cloneNode(true);
 
+    // we're going to get the image element using our querySelector on content
+    const image = content.querySelector('img');
+    // using the this.getAttribute to put this or a placeholder.
+    console.log(this.getAttribute('avatar') || 'https://placehold.co/80x80');
+    // this.getAttribute('avatar') looks at the avatar
+    // property on the html card <user-card avatar="...">...
+
+    // let's set the src of the image
+    image.src = this.getAttribute('avatar') || 'https://placehold.co/80x80';
+
     // attach this to the shadow node so we have a copy to work with
     shadow.appendChild(content);
   }
