2b8f3db7921757ec82cafd9b07d8a8f41a4c2a65
Daniel Mouris <dgmouris@gmail.com>
2025-11-13 09:44:27 -0700

    class progress pushed

--- lesson-19-starter/src/main.js
+++ lesson-19-starter/src/main.js
@@ -1,5 +1,14 @@
 // Import the user-card component to register the custom element
 import './user-card.js';
 // Create an additional user card using HTML and append it to the main element
+const dynamicUserCard = `
+  <user-card avatar="https://placehold.co/80x80/blue/yellow">
+    <span slot="name">Daniel</span>
+    <span slot="description">Our favourite zelda character</span>
+  </user-card>
+`;
+
+let main = document.querySelector('main');
+main.insertAdjacentHTML('beforeend', dynamicUserCard);
 
 // Create another user card using JavaScript DOM API only and append it to the main element
