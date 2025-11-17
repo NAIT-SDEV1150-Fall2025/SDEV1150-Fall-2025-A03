f74cffbe8ec33c3078f571a33105ae6ec0e2b614
Daniel Mouris <dgmouris@gmail.com>
2025-11-17 08:19:27 -0700

    class progress pushed

--- lesson-19-starter/src/main.js
+++ lesson-19-starter/src/main.js
@@ -12,3 +12,20 @@ let main = document.querySelector('main');
 main.insertAdjacentHTML('beforeend', dynamicUserCard);
 
 // Create another user card using JavaScript DOM API only and append it to the main element
+const anotherUserCard = document.createElement('user-card');
+// add the avatar as an attribute
+anotherUserCard.setAttribute('avatar', 'https://placehold.co/80x80/green/yellow');
+// add the name slot
+const nameSpan = document.createElement('span');
+nameSpan.setAttribute('slot', 'name');
+nameSpan.textContent = 'Yunobo';
+// addd the description slot
+const descSpan = document.createElement('span');
+descSpan.setAttribute('slot', 'description');
+descSpan.textContent = 'President of YunoboCo';
+
+// combine them
+anotherUserCard.appendChild(nameSpan);
+anotherUserCard.appendChild(descSpan);
+
+main.appendChild(anotherUserCard);
