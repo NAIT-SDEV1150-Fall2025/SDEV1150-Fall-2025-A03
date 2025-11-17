161e00b75f9a46ad8f6ccc050fe3c7a2cd35be04
Daniel Mouris <dgmouris@gmail.com>
2025-11-17 08:50:47 -0700

    class progress pushed

--- lesson-20-starter/src/main.js
+++ lesson-20-starter/src/main.js
@@ -24,3 +24,40 @@ anotherUserCard.appendChild(descSpan);
 
 document.querySelector('main').appendChild(anotherUserCard);
 // Why doesn't the custom avatar show up for this element?
+
+// we're going to
+// create a button
+const toggleBtn = document.createElement('button');
+toggleBtn.textContent = 'Toggle Theme';
+// attach it to the document.body
+document.body.prepend(toggleBtn);
+// create a boolean variable called dark
+let dark = false;
+// add click event listener
+toggleBtn.addEventListener('click', () => {
+  // set it to the opposite
+  dark = !dark;
+  let bg = '#ffffff';
+  let color = '#222222';
+  let accent = '#0077ff';
+  if (dark) {
+    bg = '#1f2937';
+    color = '#e5e7eb';
+    accent = 'gold';
+  }
+
+  // on the document.documentElement
+  // I want to you set the style property
+  // for the --global-card-bg to be
+  // #1f2937 if dark
+  // and #ffffff if white.
+  document.documentElement.style.setProperty(
+    '--global-card-bg', bg,
+  );
+  document.documentElement.style.setProperty(
+    '--global-card-color', color,
+  );
+  document.documentElement.style.setProperty(
+    '--global-card-accent', accent,
+  );
+});
