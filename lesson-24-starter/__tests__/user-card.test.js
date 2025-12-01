ca75ccaa3421098849ce2a44d9db7050812db341
Daniel Mouris <dgmouris@gmail.com>
2025-12-01 09:07:04 -0700

    class progress pushed

---
+++ lesson-24-starter/__tests__/user-card.test.js
@@ -0,0 +1,18 @@
+import {
+  expect, // a function to pass in what your'e testing
+  test, // the test itself.
+  describe, // a section of the test
+} from 'vitest';
+
+// let's think about a few
+// tests we can write
+// functionality we'll be testing
+// renders with the right properties
+test('renders with default properties', () => {
+
+});
+// renders with a name and description
+// setting an avatar attribute
+// set a user and see if the component is what we expect
+// follow and unfollow methods
+// click follow button updates state
