034f7f9ead92667ccbf1a4cb4a3b5f40cb7a3d35
Daniel Mouris <dgmouris@gmail.com>
2025-12-04 09:08:47 -0700

    class progress pushed

--- lesson-25-starter/__tests__/user-card.test.js
+++ lesson-25-starter/__tests__/user-card.test.js
@@ -48,4 +48,38 @@ describe('UserCard', () => {
     expect(nameSlot.assignedNodes()[0].textContent).toBe('Vitest User');
     expect(descSlot.assignedNodes()[0].textContent).toBe('A user for testing with Vitest');
   });
+
+  test('sets user property', () => {
+    // arrange step
+    // create an "expected" user
+    const EXPECTED_USER = {
+      id: 'test123456',
+      name: 'Daniel Steves',
+      description: 'A fake user',
+      avatar: 'http://cool.com/holo/charizard.jpg',
+    };
+    // create an element
+    const element = document.createElement('user-card');
+    // act
+    // set the user on the element  to our "expected user"
+    element.user = EXPECTED_USER;
+    // add it the page.
+    document.body.appendChild(element);
+    // assert step
+    // check avatar is set
+    const img = element.shadowRoot.querySelector('img');
+    expect(img.getAttribute('src')).toBe(EXPECTED_USER.avatar);
+    // check user id
+    expect(element.getAttribute('user-id')).toBe(EXPECTED_USER.id);
+    // check the name slot is updated
+    const nameSlot = element.shadowRoot.querySelector(
+      'slot[name="name"]',
+    );
+    expect(nameSlot.textContent).toBe(EXPECTED_USER.name);
+    // check the description slot is updated
+    const descSlot = element.shadowRoot.querySelector(
+      'slot[name="description"]',
+    );
+    expect(descSlot.textContent).toBe(EXPECTED_USER.description);
+  });
 });
